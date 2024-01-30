1593eac8646d39c4f6c15b954c3557c160b294af
root <root@ip-10-0-1-48.ap-northeast-1.compute.internal>
2024-01-30 05:09:46 +0000

    deploied function

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+  "function": {
+    "amplifyapp2c077e5a1": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
